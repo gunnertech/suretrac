a84515e9fff940ebe89d100ef9e8c4d73b1d1197
Mahdi Hussein <m@hdihusse.in>
2016-06-14 15:31:06 -0400

    Added GET for device

--- routes/devices.js
+++ routes/devices.js
@@ -28,12 +28,33 @@ Router.get('/', (req, res) => {
   });
 });
 
+// POST /devices
 Router.post('/', (req, res) => {
   (new Device).save();
   res.send();
 });
 
-// PUT /devices/id
+// GET /devices
+Router.get('/', (req, res) => {
+  Device.find({}, function(err, devices) {
+    res.send(devices);  
+  });
+});
+
+// GET /devices/:id
+Router.get('/:id', (req, res) => {
+  Device.findOne({ _id: req.params.id }, (err, device) => {
+    if (err) {
+      // TODO: ERROR
+      console.log(err);
+      return;
+    }
+
+    res.send(device);
+  });
+});
+
+// PUT /devices/:id
 Router.put('/:id', (req, res) => {
   var originalSentence = req.body["nmea-sentence"];
 
@@ -55,9 +76,6 @@ Router.put('/:id', (req, res) => {
     device.lattitude = gprmcObject.lattitude;
     device.longitude = gprmcObject.longitude;
     device.save();
-
-    console.log("Lat: " + device.lattitude);
-    console.log("Long: " + device.longitude + "\n\n");
   });
 });
 
