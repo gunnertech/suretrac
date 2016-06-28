45100627709caa59c8e9cef3fee570ad4279050f
Mahdi Hussein <m@hdihusse.in>
2016-06-28 00:47:31 -0400

    SureTrac Name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Where Are My Guns' });
+  res.render('index', { title: 'SureTrac' });
 });
 
 module.exports = router;
